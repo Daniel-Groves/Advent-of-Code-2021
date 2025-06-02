900a3ed6f48dbf4648c17a214e485dbf93df67da
u05730dg <daniel.groves@student.manchester.ac.uk>
2025-06-02 15:03:22 +0100

    Day1 part2 done

--- Day1/part2.java
+++ Day1/part2.java
@@ -4,19 +4,21 @@ import java.util.List;
 import java.nio.file.*;
 import utils.InputReader;
 
-public class part1 {
+public class part2 {
     public static void main(String[] args){
         List<String> input = InputReader.readLines("Day1/input.txt");
 
         int sum = 0;
         int last = Integer.MAX_VALUE;
-        for (String line : input) {
-            int number = Integer.parseInt(line);
-            if (number > last) {
+
+        for (int i = 2; i < input.size(); i++) {
+            int num = Integer.parseInt(input.get(i - 2)) + Integer.parseInt(input.get(i - 1)) + Integer.parseInt(input.get(i));
+            if (num > last) {
                 sum += 1;
-            }
-            last = number;
+            };
+            last = num;
         }
+
 
         System.out.println(sum);
     }
